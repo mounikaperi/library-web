205808bcf0bcbd8643672c6b0b71935f64ef0330
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-12-15 19:43:17 +0530

    Finalized Carousel

--- src/layouts/HomePage/Carousel.tsx
+++ src/layouts/HomePage/Carousel.tsx
@@ -1,35 +1,62 @@
-import React from 'react';
 import { ReturnBook } from "./ReturnBook";
 
 export const Carousel = () => {
   return (
-    <>
-      <div className='homepage-carousel-title mt-5 mb-5'>
+    <div className='container mt-5' style={{ height: 550 }}>
+      <div className='homepage-carousel-title'>
         <h3>Find your next "I stayed up too late reading" book.</h3>
       </div>
-      <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="cards-wrapper">
+      <div id='carouselExampleControls' className='carousel carousel-light slide mt-5 
+                d-none d-lg-block' data-bs-interval='false'>
+
+        {/* Desktop */}
+        <div className='carousel-inner'>
+          <div className='carousel-item active'>
+            <div className='row d-flex justify-content-center align-items-center'>
               <ReturnBook />
-              <div className="card d-none d-md-block">
-                <ReturnBook />
-              </div>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+            </div>
+          </div>
+          <div className='carousel-item'>
+            <div className='row d-flex justify-content-center align-items-center'>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+            </div>
+          </div>
+          <div className='carousel-item'>
+            <div className='row d-flex justify-content-center align-items-center'>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
             </div>
           </div>
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
+        <button className='carousel-control-prev' type='button'
+          data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+          <span className='visually-hidden'>Previous</span>
+        </button>
+        <button className='carousel-control-next' type='button'
+          data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+          <span className='carousel-control-next-icon' aria-hidden='true'></span>
+          <span className='visually-hidden'>Next</span>
+        </button>
       </div>
-      <div className='homepage-carousel-title mt-3 mb-6'>
-        <a className='btn btn-outline-secondary' href='/search'>View More</a>
+
+      {/* Mobile */}
+      <div className='d-lg-none mt-3'>
+        <div className='row d-flex justify-content-center align-items-center'>
+          <ReturnBook />
+        </div>
       </div>
-    </>
+      <div className='homepage-carousel-title mt-3'>
+        <a className='btn btn-outline-secondary btn-lg' href='/search'>View More</a>
+      </div>
+    </div>
   );
 }
