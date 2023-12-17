496fa6ca8802d4566603094753a58364483b6dfd
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-12-17 11:59:11 +0530

    Changed anchor tags to NavLinks and Links

--- src/layouts/HomePage/Carousel.tsx
+++ src/layouts/HomePage/Carousel.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { fetchBooks } from "../../services/booksService";
 import { Spinner } from "../Common/Spinner";
+import { Link } from "react-router-dom";
 
 export const Carousel = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
@@ -77,9 +78,9 @@ export const Carousel = () => {
         <div className='row d-flex justify-content-center align-items-center'>
           <ReturnBook book={books[7]} key={books[7].id} />
         </div>
-      </div>
+      </div>s
       <div className='homepage-carousel-title mt-3'>
-        <a className='btn btn-outline-secondary btn-lg' href='/search'>View More</a>
+        <Link className='btn btn-outline-secondary btn-lg' to='/search'>View More</Link>
       </div>
     </div>
   );
--- src/layouts/NavBarAndFooter/Navbar.tsx
+++ src/layouts/NavBarAndFooter/Navbar.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <>
@@ -11,8 +13,8 @@ export const Navbar = () => {
           </button>
           <div className='collapse navbar-collapse' id='navbarNavDropdown'>
             <ul className='navbar-nav'>
-              <li className='nav-item'><a className='nav-link' href='#'>Home</a></li>
-              <li className='nav-item'><a className='nav-link' href='#'>Search Books</a></li>
+              <li className='nav-item'><NavLink className='nav-link' to='/home'>Home</NavLink></li>
+              <li className='nav-item'><NavLink className='nav-link' to='/search'>Search Books</NavLink></li>
             </ul>
             <ul className='navbar-nav ms-auto'>
               <li className='nav-item m-1'><a type='button' className='btn btn-outline-light' href='#'>Sign In</a></li>
